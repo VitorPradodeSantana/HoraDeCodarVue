8faf51d76d1d6c3bb76773111b9432b85007277f
Vitor Prado de Santana <59122547+VitorPradodeSantana@users.noreply.github.com>
2023-07-25 22:53:51 -0300

    Subindo projeto para o GIT

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Olá vue!</h1>
+    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'PrimeiroComponente',
+    data() {
+      return {
+          nome: "Vitor",
+          profissao: "Programador"
+      }
+    }
+  }
+</script>
